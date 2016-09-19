37e2910a67dbb1138766d66a15a438111d3bcce1
astroJeff <andrews@astro.columbia.edu>
2016-09-19 16:37:55 +0300

    Added a test for profiling

---
+++ src/profile_test.py
@@ -0,0 +1,45 @@
+import numpy as np
+from astropy.table import Table
+import pickle
+import sys
+sys.path.append('../src')
+import P_posterior
+
+
+# Read in sample from TGAS table
+dtype = [('ID','<i8'),('tyc','S11'),('hip','<i8'),('ra','<f8'),('dec','<f8'),('mu_ra','<f8'),('mu_dec','<f8'), \
+     ('mu_ra_err','<f8'),('mu_dec_err','<f8'),('plx','<f8'),('plx_err','<f8'),('d_Q','<f8'),('noise','<f8')]
+
+tgas_full = np.array([], dtype=dtype)
+
+for i in np.arange(1):
+    if i < 10:
+        filename = ('../data/TGAS/TgasSource_000-000-00' + str(i) + '.csv')
+    else:
+        filename = ('../data/TGAS/TgasSource_000-000-0' + str(i) + '.csv')
+
+    print filename
+    tgas_tmp = Table.read(filename, format='csv', guess=True)
+
+
+
+    tgas = np.zeros(len(tgas_tmp), dtype=dtype)
+                                                  # Gaia units
+    tgas['ID'] = tgas_tmp['source_id']
+    tgas['tyc'] = tgas_tmp['tycho2_id']
+    tgas['hip'] = tgas_tmp['hip']
+    tgas['ra'] = tgas_tmp['ra']                   # degrees
+    tgas['dec'] = tgas_tmp['dec']                 # degrees
+    tgas['mu_ra'] = tgas_tmp['pmra']              # mas/yr
+    tgas['mu_ra_err'] = tgas_tmp['pmra_error']    # mas/yr
+    tgas['mu_dec'] = tgas_tmp['pmdec']            # mas/yr
+    tgas['mu_dec_err'] = tgas_tmp['pmdec_error']  # mas/yr
+    tgas['plx'] = tgas_tmp['parallax']            # mas
+    tgas['plx_err'] = tgas_tmp['parallax_error']  # mas
+    tgas['d_Q'] = tgas_tmp['astrometric_delta_q']
+    tgas['noise'] = tgas_tmp['astrometric_excess_noise_sig']
+
+    tgas_full = np.append(tgas_full, tgas)
+
+
+prob_out = P_posterior.match_binaries(tgas_full, sys_start=50007, subsample=100, size_integrate_full=100000, size_integrate_plx=100000)
