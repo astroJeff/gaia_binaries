02683d6168e964141953c55a0980f36885abef7f
astroJeff <andrews@astro.columbia.edu>
2016-10-23 12:16:31 +0300

    Updated catalog cross matching

--- src/create_catalog.py
+++ src/create_catalog.py
@@ -2,6 +2,13 @@ import numpy as np
 from astropy.table import Table
 import pickle
 import os
+import sys
+
+
+
+if len(sys.argv) < 2:
+     print "You must provide command line arguments"
+     exit(-1)
 
 
 
@@ -50,13 +57,20 @@ for i in np.arange(16):
 
 
 
+######## READ IN TGAS CATALOG WITH CROSS MATCHES ########
+TGAS_combined = np.load('../data/TGAS/TGAS_combined.npy')
+
+
+
 
 
 ######### READ IN MATCHED PAIRS ###########
 
-folder = '../data/TGAS/'
+model = sys.argv[1]
+folder = '../data/TGAS/' + sys.argv[1] + '/'
 
-TGAS_tmp = pickle.load(open(folder + 'TGAS_match_1.p', 'rb'))
+
+TGAS_tmp = pickle.load(open(folder + model+'_1.p', 'rb'))
 
 TGAS = np.array([], dtype=TGAS_tmp.dtype)
 
@@ -67,7 +81,7 @@ for filename in os.listdir(folder):
         TGAS = np.append(TGAS, TGAS_tmp)
 
 
-
+print len(TGAS)
 
 
 
@@ -82,49 +96,148 @@ TGAS_good = TGAS[TGAS['P_posterior'] > 0.01]
 
 ######### CREATE NEW CATALOG ##############
 
-dtype = [('source_id_1','<i8'), ('ra_1','f8'), ('dec_1','f8'), \
-         ('source_id_2','<i8'), ('ra_2','f8'), ('dec_2','f8'), \
-         ('mu_ra_1','f8'), ('mu_dec_1','f8'), ('mu_ra_err_1','f8'), ('mu_dec_err_1','f8'), \
+dtype = [('P_posterior','f8'), ('theta','f8'),
+         ('source_id_1','<i8'), ('TYC_id_1','S11'), ('hip_id_1','<i8'),
+         ('ra_1','f8'), ('dec_1','f8'),
+         ('mu_ra_1','f8'), ('mu_dec_1','f8'), ('mu_ra_err_1','f8'), ('mu_dec_err_1','f8'),
+         ('plx_1','f8'), ('plx_err_1','f8'),
+         ('gaia_g_flux_1','<f8'), ('gaia_g_flux_err_1','<f8'), ('gaia_g_mag_1','<f8'),
+         ('TMASS_id_1','<i8'), ('TMASS_angle_dist_1','<f8'),
+         ('TMASS_n_neighbours_1','i8'), ('TMASS_n_mates_1','i8'), ('TMASS_ph_qual_1','S11'),
+         ('TMASS_ra_1','<f8'), ('TMASS_dec_1','<f8'),
+         ('TMASS_j_mag_1','<f8'), ('TMASS_j_mag_err_1','<f8'),
+         ('TMASS_h_mag_1','<f8'), ('TMASS_h_mag_err_1','<f8'),
+         ('TMASS_ks_mag_1','<f8'), ('TMASS_ks_mag_err_1','<f8'),
+         ('TYC_Vt_1','<f8'), ('TYC_Vt_err_1','<f8'),
+         ('TYC_Bt_1','<f8'), ('TYC_Bt_err_1','<f8'),
+         ('gaia_delta_Q_1','<f8'), ('gaia_noise_1','<f8'),
+         #
+         ('source_id_2','<i8'), ('TYC_id_2','S11'), ('hip_id_2','<i8'),
+         ('ra_2','f8'), ('dec_2','f8'),
          ('mu_ra_2','f8'), ('mu_dec_2','f8'), ('mu_ra_err_2','f8'), ('mu_dec_err_2','f8'),
-         ('plx_1','f8'), ('plx_err_1','f8'), ('plx_2','f8'), ('plx_err_2','f8'), \
-         ('P_posterior','f8'), ('theta','f8')]
+         ('plx_2','f8'), ('plx_err_2','f8'),
+         ('gaia_g_flux_2','<f8'), ('gaia_g_flux_err_2','<f8'), ('gaia_g_mag_2','<f8'),
+         ('TMASS_id_2','<i8'), ('TMASS_angle_dist_2','<f8'),
+         ('TMASS_n_neighbours_2','i8'), ('TMASS_n_mates_2','i8'), ('TMASS_ph_qual_2','S11'),
+         ('TMASS_ra_2','<f8'), ('TMASS_dec_2','<f8'),
+         ('TMASS_j_mag_2','<f8'), ('TMASS_j_mag_err_2','<f8'),
+         ('TMASS_h_mag_2','<f8'), ('TMASS_h_mag_err_2','<f8'),
+         ('TMASS_ks_mag_2','<f8'), ('TMASS_ks_mag_err_2','<f8'),
+         ('TYC_Vt_2','<f8'), ('TYC_Vt_err_2','<f8'),
+         ('TYC_Bt_2','<f8'), ('TYC_Bt_err_2','<f8'),
+         ('gaia_delta_Q_2','<f8'), ('gaia_noise_2','<f8')
+        ]
 
-pairs = np.zeros(len(TGAS_good), dtype=dtype)
 
+length = len(TGAS_good)
+pairs = np.zeros(length, dtype=dtype)
 
-for i in np.arange(len(TGAS_good)):
+for i in np.arange(length):
+
+    idx1 = np.where(TGAS_combined['source_id'] == tgas_full['ID'][TGAS_good['i_1'][i]])
+    idx2 = np.where(TGAS_combined['source_id'] == tgas_full['ID'][TGAS_good['i_2'][i]])
+
+    # if len(idx1) > 1 or len(idx2) > 1: print i, len(idx1), len(idx2)
+
+    print
+
+    pairs['P_posterior'][i] = TGAS_good['P_posterior'][i]
+    pairs['theta'][i] = TGAS_good['theta'][i] * 3600.0
 
     pairs['source_id_1'][i] = tgas_full['ID'][TGAS_good['i_1'][i]]
+    pairs['TYC_id_1'][i] = tgas_full['tyc'][TGAS_good['i_1'][i]]
+    pairs['hip_id_1'][i] = tgas_full['hip'][TGAS_good['i_1'][i]]
     pairs['ra_1'][i] = tgas_full['ra'][TGAS_good['i_1'][i]]
     pairs['dec_1'][i] = tgas_full['dec'][TGAS_good['i_1'][i]]
-    pairs['source_id_2'][i] = tgas_full['ID'][TGAS_good['i_2'][i]]
-    pairs['ra_2'][i] = tgas_full['ra'][TGAS_good['i_2'][i]]
-    pairs['dec_2'][i] = tgas_full['dec'][TGAS_good['i_2'][i]]
     pairs['mu_ra_1'][i] = tgas_full['mu_ra'][TGAS_good['i_1'][i]]
     pairs['mu_dec_1'][i] = tgas_full['mu_dec'][TGAS_good['i_1'][i]]
     pairs['mu_ra_err_1'][i] = tgas_full['mu_ra_err'][TGAS_good['i_1'][i]]
     pairs['mu_dec_err_1'][i] = tgas_full['mu_dec_err'][TGAS_good['i_1'][i]]
+    pairs['plx_1'][i] = tgas_full['plx'][TGAS_good['i_1'][i]]
+    pairs['plx_err_1'][i] = tgas_full['plx_err'][TGAS_good['i_1'][i]]
+    pairs['gaia_g_flux_1'][i] = TGAS_combined['gaia_g_flux'][idx1]
+    pairs['gaia_g_flux_err_1'][i] = TGAS_combined['gaia_g_flux_error'][idx1]
+    pairs['gaia_g_mag_1'][i] = TGAS_combined['gaia_g_mag'][idx1]
+    pairs['TMASS_id_1'][i] = TGAS_combined['TMASS_id'][idx1]
+    pairs['TMASS_angle_dist_1'][i] = TGAS_combined['TMASS_angle_dist'][idx1]
+    pairs['TMASS_n_neighbours_1'][i] = TGAS_combined['TMASS_n_neighbours'][idx1]
+    pairs['TMASS_n_mates_1'][i] = TGAS_combined['TMASS_n_mates'][idx1]
+    pairs['TMASS_ph_qual_1'][i] = str(TGAS_combined['TMASS_ph_qual'][idx1])
+    pairs['TMASS_ra_1'][i] = TGAS_combined['TMASS_ra'][idx1]
+    pairs['TMASS_dec_1'][i] = TGAS_combined['TMASS_dec'][idx1]
+    pairs['TMASS_j_mag_1'][i] = TGAS_combined['TMASS_j_mag'][idx1]
+    pairs['TMASS_j_mag_err_1'][i] = TGAS_combined['TMASS_j_mag_error'][idx1]
+    pairs['TMASS_h_mag_1'][i] = TGAS_combined['TMASS_h_mag'][idx1]
+    pairs['TMASS_h_mag_err_1'][i] = TGAS_combined['TMASS_h_mag_error'][idx1]
+    pairs['TMASS_ks_mag_1'][i] = TGAS_combined['TMASS_ks_mag'][idx1]
+    pairs['TMASS_ks_mag_err_1'][i] = TGAS_combined['TMASS_ks_mag_error'][idx1]
+    pairs['TYC_Vt_1'][i] = TGAS_combined['TYC_Vt'][idx1]
+    pairs['TYC_Vt_err_1'][i] = TGAS_combined['TYC_Vt_err'][idx1]
+    pairs['TYC_Bt_1'][i] = TGAS_combined['TYC_Bt'][idx1]
+    pairs['TYC_Bt_err_1'][i] = TGAS_combined['TYC_Bt_err'][idx1]
+    pairs['gaia_delta_Q_1'][i] = tgas_full['d_Q'][TGAS_good['i_1'][i]]
+    pairs['gaia_noise_1'][i] = tgas_full['noise'][TGAS_good['i_1'][i]]
+
+    pairs['source_id_2'][i] = tgas_full['ID'][TGAS_good['i_2'][i]]
+    pairs['TYC_id_2'][i] = tgas_full['tyc'][TGAS_good['i_2'][i]]
+    pairs['hip_id_2'][i] = tgas_full['hip'][TGAS_good['i_2'][i]]
+    pairs['ra_2'][i] = tgas_full['ra'][TGAS_good['i_2'][i]]
+    pairs['dec_2'][i] = tgas_full['dec'][TGAS_good['i_2'][i]]
     pairs['mu_ra_2'][i] = tgas_full['mu_ra'][TGAS_good['i_2'][i]]
     pairs['mu_dec_2'][i] = tgas_full['mu_dec'][TGAS_good['i_2'][i]]
     pairs['mu_ra_err_2'][i] = tgas_full['mu_ra_err'][TGAS_good['i_2'][i]]
     pairs['mu_dec_err_2'][i] = tgas_full['mu_dec_err'][TGAS_good['i_2'][i]]
-    pairs['plx_1'][i] = tgas_full['plx'][TGAS_good['i_1'][i]]
-    pairs['plx_err_1'][i] = tgas_full['plx_err'][TGAS_good['i_1'][i]]
     pairs['plx_2'][i] = tgas_full['plx'][TGAS_good['i_2'][i]]
     pairs['plx_err_2'][i] = tgas_full['plx_err'][TGAS_good['i_2'][i]]
-    pairs['P_posterior'][i] = TGAS_good['P_posterior'][i]
-    pairs['theta'][i] = TGAS_good['theta'][i] * 3600.0
+    pairs['gaia_g_flux_2'][i] = TGAS_combined['gaia_g_flux'][idx2]
+    pairs['gaia_g_flux_err_2'][i] = TGAS_combined['gaia_g_flux_error'][idx2]
+    pairs['gaia_g_mag_2'][i] = TGAS_combined['gaia_g_mag'][idx2]
+    pairs['TMASS_id_2'][i] = TGAS_combined['TMASS_id'][idx2]
+    pairs['TMASS_angle_dist_2'][i] = TGAS_combined['TMASS_angle_dist'][idx2]
+    pairs['TMASS_n_neighbours_2'][i] = TGAS_combined['TMASS_n_neighbours'][idx2]
+    pairs['TMASS_n_mates_2'][i] = TGAS_combined['TMASS_n_mates'][idx2]
+    pairs['TMASS_ph_qual_2'][i] = str(TGAS_combined['TMASS_ph_qual'][idx2])
+    pairs['TMASS_ra_2'][i] = TGAS_combined['TMASS_ra'][idx2]
+    pairs['TMASS_dec_2'][i] = TGAS_combined['TMASS_dec'][idx2]
+    pairs['TMASS_j_mag_2'][i] = TGAS_combined['TMASS_j_mag'][idx2]
+    pairs['TMASS_j_mag_err_2'][i] = TGAS_combined['TMASS_j_mag_error'][idx2]
+    pairs['TMASS_h_mag_2'][i] = TGAS_combined['TMASS_h_mag'][idx2]
+    pairs['TMASS_h_mag_err_2'][i] = TGAS_combined['TMASS_h_mag_error'][idx2]
+    pairs['TMASS_ks_mag_2'][i] = TGAS_combined['TMASS_ks_mag'][idx2]
+    pairs['TMASS_ks_mag_err_2'][i] = TGAS_combined['TMASS_ks_mag_error'][idx2]
+    pairs['TYC_Vt_2'][i] = TGAS_combined['TYC_Vt'][idx2]
+    pairs['TYC_Vt_err_2'][i] = TGAS_combined['TYC_Vt_err'][idx2]
+    pairs['TYC_Bt_2'][i] = TGAS_combined['TYC_Bt'][idx2]
+    pairs['TYC_Bt_err_2'][i] = TGAS_combined['TYC_Bt_err'][idx2]
+    pairs['gaia_delta_Q_2'][i] = tgas_full['d_Q'][TGAS_good['i_2'][i]]
+    pairs['gaia_noise_2'][i] = tgas_full['noise'][TGAS_good['i_2'][i]]
 
 
 
 
-header = 'source_ID_1 ra_1 dec_1 source_ID_2 ra_2 dec_2 mu_ra_1 mu_dec_1 mu_ra_err_1 mu_dec_err_1' + \
-         ' mu_ra_2 mu_dec_2 mu_ra_err_2 mu_dec_err_2 plx_1 plx_err_1 plx_2 plx_err_2' + \
-         ' P_posterior theta' + \
+header = 'P_posterior theta ' + \
+         'source_ID_1 TYC_ID_1 HIP_ID_1 ra_1 dec_1  mu_ra_1 mu_dec_1 mu_ra_err_1 mu_dec_err_1 plx_1 plx_err_1 ' + \
+         'gaia_g_flux_1 gaia_g_flux_err_1 gaia_g_mag_1 ' + \
+         '2MASS_ID_1 2MASS_angle_dist_1 2MASS_n_neighbours_1 2MASS_n_mates_1 2MASS_ph_qual_1 ' + \
+         '2MASS_ra_1 2MASS_dec_1 ' + \
+         '2MASS_j_mag_1 2MASS_j_mag_err_1 2MASS_h_mag_1 2MASS_h_mag_err_1 2MASS_ks_mag_1 2MASS_ks_mag_err_1 ' + \
+         'Tycho_Vt_1 Tycho_Vt_err_1 Tycho_Bt_1 Tycho_Bt_err_1 ' + \
+         'Gaia_delta_Q_1 Gaia_noise_1 ' + \
+         'source_ID_2 TYC_ID_2 HIP_ID_2 ra_2 dec_2  mu_ra_2 mu_dec_2 mu_ra_err_2 mu_dec_err_2 plx_2 plx_err_2 ' + \
+         'gaia_g_flux_2 gaia_g_flux_err_2 gaia_g_mag_2 ' + \
+         '2MASS_ID_2 2MASS_angle_dist_2 2MASS_n_neighbours_2 2MASS_n_mates_2 2MASS_ph_qual_2 ' + \
+         '2MASS_ra_2 2MASS_dec_2 ' + \
+         '2MASS_j_mag_2 2MASS_j_mag_err_2 2MASS_h_mag_2 2MASS_h_mag_err_2 2MASS_ks_mag_2 2MASS_ks_mag_err_2 ' + \
+         'Tycho_Vt_2 Tycho_Vt_err_2 Tycho_Bt_2 Tycho_Bt_err_2 ' + \
+         'Gaia_delta_Q_2 Gaia_noise_2 ' + \
          '\nPositions are in degrees' + \
          '\nProper motions are in mas/yr' + \
          '\nParallaxes are in mas'
 
-format = '%i %1.9f %1.9f %i %1.9f %1.9f %1.3f %1.3f %1.3f %1.3f %1.3f %1.3f %1.3f %1.3f %1.3f %1.3f %1.3f %1.3f %.3e %1.2f'  
+format = '%.3e %1.2f ' + \
+         '%i %s %i %1.9f %1.9f %1.3f %1.3f %1.3f %1.3f %1.3f %1.3f %1.3e %1.3e %1.2f ' + \
+         '%i %1.3f %i %i %s %1.9f %1.9f %1.2f %1.2f %1.2f %1.2f %1.2f %1.2f %1.2f %1.2f %1.2f %1.2f %1.2f %1.2f ' + \
+         '%i %s %i %1.9f %1.9f %1.3f %1.3f %1.3f %1.3f %1.3f %1.3f %1.3e %1.3e %1.2f ' + \
+         '%i %1.3f %i %i %s %1.9f %1.9f %1.2f %1.2f %1.2f %1.2f %1.2f %1.2f %1.2f %1.2f %1.2f %1.2f %1.2f %1.2f'
 
-np.savetxt('../data/TGAS/gaia_wide_binaries.txt', pairs, delimiter=' ', header=header, fmt=format)
+np.savetxt('../data/TGAS/'+'gaia_wide_binaries_'+model+'.txt', pairs, delimiter=' ', header=header, fmt=format)
--- src/cross_match.py
+++ src/cross_match.py
@@ -10,7 +10,7 @@ dtype = [('source_id','<i8'),('tyc','S11'),('hip','<i8'),
          ('ra','<f8'),('dec','<f8'),
          ('mu_ra','<f8'),('mu_dec','<f8'), ('mu_ra_err','<f8'),('mu_dec_err','<f8'),
          ('plx','<f8'),('plx_err','<f8'),
-         ('gaia_g_flux','<f8'), ('gaia_g_flux_error','<f8')
+         ('gaia_g_flux','<f8'), ('gaia_g_flux_error','<f8'), ('gaia_g_mag','<f8')
         ]
 
 tgas_full = np.array([], dtype=dtype)
@@ -41,22 +41,30 @@ for i in np.arange(16):
     tgas['plx_err'] = tgas_tmp['parallax_error']
     tgas['gaia_g_flux'] = tgas_tmp['phot_g_mean_flux']
     tgas['gaia_g_flux_error'] = tgas_tmp['phot_g_mean_flux_error']
+    tgas['gaia_g_mag'] = tgas_tmp['phot_g_mean_mag']
 
     tgas_full = np.append(tgas_full, tgas)
 
 
-# URAT Catalog
-filename = ('../data/TGAS/TGAS_URAT.vot')
-URAT = Table.read(filename, format='votable')
+# # URAT Catalog
+# filename = ('../data/TGAS/TGAS_URAT.vot')
+# URAT = Table.read(filename, format='votable')
 
 # 2MASS Catalog
 filename = ('../data/TGAS/TGAS_2MASS.vot')
 TWO_MASS = Table.read(filename, format='votable')
 
-# ppmxl Catalog
-filename = ('../data/TGAS/TGAS_ppmxl.vot')
-ppmxl = Table.read(filename, format='votable')
+# # ppmxl Catalog
+# filename = ('../data/TGAS/TGAS_ppmxl.vot')
+# ppmxl = Table.read(filename, format='votable')
 
+# Tycho-2 Catalog
+filename = ('../data/tycho-2/tyc2.dat')
+readme = ('../data/tycho-2/ReadMe')
+TYC = Table.read(filename, format='cds', guess=False, readme=readme)
+TYC_ID = np.zeros(len(TYC), dtype='S11')
+for i in np.arange(len(TYC)):
+    TYC_ID[i] = str(TYC['TYC1'][i]) + '-' + str(TYC['TYC2'][i]) + '-' + str(TYC['TYC3'][i])
 
 
 # Now, match the catalogs to create a combined master
@@ -66,25 +74,28 @@ dtype = [('source_id','<i8'),('tyc','S11'),('hip','<i8'),
          ('mu_ra','<f8'),('mu_dec','<f8'), ('mu_ra_err','<f8'),('mu_dec_err','<f8'),
          ('plx','<f8'),('plx_err','<f8'),
          ('gaia_g_flux','<f8'), ('gaia_g_flux_error','<f8'),
-         ('URAT_id','<i8'), ('URAT_angle_dist','<f8'),
-         ('URAT_ra','<f8'), ('URAT_dec','<f8'),
-         ('URAT_f_mag','<f8'), ('URAT_f_mag_error','<f8'),
-         ('URAT_b_mag','<f8'), ('URAT_b_mag_error','<f8'),
-         ('URAT_v_mag','<f8'), ('URAT_v_mag_error','<f8'),
-         ('URAT_g_mag','<f8'), ('URAT_g_mag_error','<f8'),
-         ('URAT_r_mag','<f8'), ('URAT_r_mag_error','<f8'),
-         ('URAT_i_mag','<f8'), ('URAT_i_mag_error','<f8'),
+         ('gaia_g_mag','<f8'),
+        #  ('URAT_id','<i8'), ('URAT_angle_dist','<f8'),
+        #  ('URAT_ra','<f8'), ('URAT_dec','<f8'),
+        #  ('URAT_f_mag','<f8'), ('URAT_f_mag_error','<f8'),
+        #  ('URAT_b_mag','<f8'), ('URAT_b_mag_error','<f8'),
+        #  ('URAT_v_mag','<f8'), ('URAT_v_mag_error','<f8'),
+        #  ('URAT_g_mag','<f8'), ('URAT_g_mag_error','<f8'),
+        #  ('URAT_r_mag','<f8'), ('URAT_r_mag_error','<f8'),
+        #  ('URAT_i_mag','<f8'), ('URAT_i_mag_error','<f8'),
          ('TMASS_id','<i8'), ('TMASS_angle_dist','<f8'),
+         ('TMASS_n_neighbours','i8'), ('TMASS_n_mates','i8'), ('TMASS_ph_qual','S11'), 
          ('TMASS_ra','<f8'), ('TMASS_dec','<f8'),
          ('TMASS_j_mag','<f8'), ('TMASS_j_mag_error','<f8'),
          ('TMASS_h_mag','<f8'), ('TMASS_h_mag_error','<f8'),
          ('TMASS_ks_mag','<f8'), ('TMASS_ks_mag_error','<f8'),
-         ('ppmxl_id','<i8'), ('ppmxl_angle_dist','<f8'),
-         ('ppmxl_ra','<f8'), ('ppmxl_dec','<f8'),
-         ('ppmxl_b1_mag','<f8'), ('ppmxl_b2_mag','<f8'),
-         ('ppmxl_r1_mag','<f8'), ('ppmxl_i_mag','<f8')
+         ('TYC_Vt','<f8'), ('TYC_Vt_err','<f8'),
+         ('TYC_Bt','<f8'), ('TYC_Bt_err','<f8')
+        #  ('ppmxl_id','<i8'), ('ppmxl_angle_dist','<f8'),
+        #  ('ppmxl_ra','<f8'), ('ppmxl_dec','<f8'),
+        #  ('ppmxl_b1_mag','<f8'), ('ppmxl_b2_mag','<f8'),
+        #  ('ppmxl_r1_mag','<f8'), ('ppmxl_i_mag','<f8')
         ]
-
 
 
 num = len(tgas_full)
@@ -98,9 +109,10 @@ for i in np.arange(num):
     if i%1000 == 0: print i, time.time()-start
 
 
-    idx1 = np.where(tgas_full[i]['source_id'] == URAT['source_id'])
+    # idx1 = np.where(tgas_full[i]['source_id'] == URAT['source_id'])
     idx2 = np.where(tgas_full[i]['source_id'] == TWO_MASS['source_id'])
-    idx3 = np.where(tgas_full[i]['source_id'] == ppmxl['source_id'])
+    # idx3 = np.where(tgas_full[i]['source_id'] == ppmxl['source_id'])
+    idx4 = np.where(tgas_full[i]['tyc'] == TYC_ID)
 
 
     tgas_full_combined[i]['source_id'] = tgas_full[i]['source_id']
@@ -116,32 +128,35 @@ for i in np.arange(num):
     tgas_full_combined[i]['plx_err'] = tgas_full[i]['plx_err']
     tgas_full_combined[i]['gaia_g_flux'] = tgas_full[i]['gaia_g_flux']
     tgas_full_combined[i]['gaia_g_flux_error'] = tgas_full[i]['gaia_g_flux_error']
+    tgas_full_combined[i]['gaia_g_mag'] = (tgas_full[i]['gaia_g_mag'])
 
 
-
-
-    if len(idx1[0] > 0):
-        tgas_full_combined[i]['URAT_id'] = URAT[idx1]['urat1_oid']
-        tgas_full_combined[i]['URAT_angle_dist'] = URAT[idx1]['angular_distance']
-        tgas_full_combined[i]['URAT_ra'] = URAT[idx1]['ra']
-        tgas_full_combined[i]['URAT_dec'] = URAT[idx1]['dec']
-        tgas_full_combined[i]['URAT_f_mag'] = URAT[idx1]['f_mag']
-        tgas_full_combined[i]['URAT_f_mag_error'] = URAT[idx1]['f_mag_error']
-        tgas_full_combined[i]['URAT_b_mag'] = URAT[idx1]['b_mag']
-        tgas_full_combined[i]['URAT_b_mag_error'] = URAT[idx1]['b_mag_error']
-        tgas_full_combined[i]['URAT_v_mag'] = URAT[idx1]['v_mag']
-        tgas_full_combined[i]['URAT_v_mag_error'] = URAT[idx1]['v_mag_error']
-        tgas_full_combined[i]['URAT_g_mag'] = URAT[idx1]['g_mag']
-        tgas_full_combined[i]['URAT_g_mag_error'] = URAT[idx1]['g_mag_error']
-        tgas_full_combined[i]['URAT_r_mag'] = URAT[idx1]['r_mag']
-        tgas_full_combined[i]['URAT_r_mag_error'] = URAT[idx1]['r_mag_error']
-        tgas_full_combined[i]['URAT_i_mag'] = URAT[idx1]['i_mag']
-        tgas_full_combined[i]['URAT_i_mag_error'] = URAT[idx1]['i_mag_error']
+    #
+    # if len(idx1[0] > 0):
+    #     tgas_full_combined[i]['URAT_id'] = URAT[idx1]['urat1_oid']
+    #     tgas_full_combined[i]['URAT_angle_dist'] = URAT[idx1]['angular_distance']
+    #     tgas_full_combined[i]['URAT_ra'] = URAT[idx1]['ra']
+    #     tgas_full_combined[i]['URAT_dec'] = URAT[idx1]['dec']
+    #     tgas_full_combined[i]['URAT_f_mag'] = URAT[idx1]['f_mag']
+    #     tgas_full_combined[i]['URAT_f_mag_error'] = URAT[idx1]['f_mag_error']
+    #     tgas_full_combined[i]['URAT_b_mag'] = URAT[idx1]['b_mag']
+    #     tgas_full_combined[i]['URAT_b_mag_error'] = URAT[idx1]['b_mag_error']
+    #     tgas_full_combined[i]['URAT_v_mag'] = URAT[idx1]['v_mag']
+    #     tgas_full_combined[i]['URAT_v_mag_error'] = URAT[idx1]['v_mag_error']
+    #     tgas_full_combined[i]['URAT_g_mag'] = URAT[idx1]['g_mag']
+    #     tgas_full_combined[i]['URAT_g_mag_error'] = URAT[idx1]['g_mag_error']
+    #     tgas_full_combined[i]['URAT_r_mag'] = URAT[idx1]['r_mag']
+    #     tgas_full_combined[i]['URAT_r_mag_error'] = URAT[idx1]['r_mag_error']
+    #     tgas_full_combined[i]['URAT_i_mag'] = URAT[idx1]['i_mag']
+    #     tgas_full_combined[i]['URAT_i_mag_error'] = URAT[idx1]['i_mag_error']
 
 
     if len(idx2[0] > 0):
         tgas_full_combined[i]['TMASS_id'] = TWO_MASS[idx2]['tmass_oid']
         tgas_full_combined[i]['TMASS_angle_dist'] = TWO_MASS[idx2]['angular_distance']
+        tgas_full_combined[i]['TMASS_n_neighbours'] = TWO_MASS[idx2]['best_neighbour_multiplicity'] 
+        tgas_full_combined[i]['TMASS_n_mates'] = TWO_MASS[idx2]['number_of_mates'] 
+        tgas_full_combined[i]['TMASS_ph_qual'] = TWO_MASS[idx2]['ph_qual'][0]  
         tgas_full_combined[i]['TMASS_ra'] = TWO_MASS[idx2]['ra']
         tgas_full_combined[i]['TMASS_dec'] = TWO_MASS[idx2]['dec']
         tgas_full_combined[i]['TMASS_j_mag'] = TWO_MASS[idx2]['j_m']
@@ -151,17 +166,21 @@ for i in np.arange(num):
         tgas_full_combined[i]['TMASS_ks_mag'] = TWO_MASS[idx2]['ks_m']
         tgas_full_combined[i]['TMASS_ks_mag_error'] = TWO_MASS[idx2]['ks_msigcom']
 
-    if len(idx3[0] > 0):
-        tgas_full_combined[i]['ppmxl_id'] = ppmxl[idx3]['ppmxl_oid']
-        tgas_full_combined[i]['ppmxl_angle_dist'] = ppmxl[idx3]['angular_distance']
-        tgas_full_combined[i]['ppmxl_ra'] = ppmxl[idx3]['ra']
-        tgas_full_combined[i]['ppmxl_dec'] = ppmxl[idx3]['dec']
-        tgas_full_combined[i]['ppmxl_b1_mag'] = ppmxl[idx3]['b1mag']
-        tgas_full_combined[i]['ppmxl_b2_mag'] = ppmxl[idx3]['b2mag']
-        tgas_full_combined[i]['ppmxl_r1_mag'] = ppmxl[idx3]['r1mag']
-        tgas_full_combined[i]['ppmxl_i_mag'] = ppmxl[idx3]['imag']
+    # if len(idx3[0] > 0):
+    #     tgas_full_combined[i]['ppmxl_id'] = ppmxl[idx3]['ppmxl_oid']
+    #     tgas_full_combined[i]['ppmxl_angle_dist'] = ppmxl[idx3]['angular_distance']
+    #     tgas_full_combined[i]['ppmxl_ra'] = ppmxl[idx3]['ra']
+    #     tgas_full_combined[i]['ppmxl_dec'] = ppmxl[idx3]['dec']
+    #     tgas_full_combined[i]['ppmxl_b1_mag'] = ppmxl[idx3]['b1mag']
+    #     tgas_full_combined[i]['ppmxl_b2_mag'] = ppmxl[idx3]['b2mag']
+    #     tgas_full_combined[i]['ppmxl_r1_mag'] = ppmxl[idx3]['r1mag']
+    #     tgas_full_combined[i]['ppmxl_i_mag'] = ppmxl[idx3]['imag']
 
-
+    if len(idx4[0] > 0):
+        tgas_full_combined[i]['TYC_Vt'] = TYC['VTmag'][idx4]
+        tgas_full_combined[i]['TYC_Vt_err'] = TYC['e_VTmag'][idx4]
+        tgas_full_combined[i]['TYC_Bt'] = TYC['BTmag'][idx4]
+        tgas_full_combined[i]['TYC_Bt_err'] = TYC['e_BTmag'][idx4]
 
 
 # Save the catalog to a np array
