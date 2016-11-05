Fixed distance calculation so it goes across 360 deg - 0 deg RA border

src/P_random.py
@@ -44,9 +44,12 @@ def get_theta_proj_degree(ra, dec, ra_b, dec_b):
     ra2 = deg_to_rad(ra_b)
     dec2 = deg_to_rad(dec_b)
 
-    dist = np.sqrt((ra1-ra2)**2 * np.cos(dec1)*np.cos(dec2) + (dec1-dec2)**2)
+    dist = np.ones((3, len(np.atleast_1d(ra))))
+    dist[0] = np.sqrt((ra1-ra2)**2 * np.cos(dec1)*np.cos(dec2) + (dec1-dec2)**2)
+    dist[1] = np.sqrt((ra1-ra2-2.0*np.pi)**2 * np.cos(dec1)*np.cos(dec2) + (dec1-dec2)**2)
+    dist[2] = np.sqrt((ra1-ra2+2.0*np.pi)**2 * np.cos(dec1)*np.cos(dec2) + (dec1-dec2)**2)
 
-    return rad_to_deg(dist)
+    return rad_to_deg(np.min(dist, axis=0))
 
 
 def get_delta_mu(mu_ra, mu_dec, mu_ra_b, mu_dec_b):
@@ -100,6 +103,7 @@ def nstars_nearby(ra, dec, radius=1.0, catalog=None):
         print "You must supply an input catalog"
         return
 
+    # Note: Function does not work across 360 deg - 0 deg border 
 
     ra_rad1 = deg_to_rad(ra)
     dec_rad1 = deg_to_rad(dec)