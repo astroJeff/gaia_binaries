6e48d302c9ab37b966ca518fae64acd463ece24d
astroJeff <andrews@astro.columbia.edu>
2016-11-07 10:37:22 +0200

    Added a backlog of notebooks and .tex updates

--- src/TGAS.py
+++ src/TGAS.py
@@ -20,7 +20,7 @@ for i in np.arange(16):
         filename = ('../data/TGAS/TgasSource_000-000-00' + str(i) + '.csv')
     else:
         filename = ('../data/TGAS/TgasSource_000-000-0' + str(i) + '.csv')
-
+        
     print filename
     tgas_tmp = Table.read(filename, format='csv', guess=True)
 
