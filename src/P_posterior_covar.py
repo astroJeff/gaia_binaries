8760584a95d60eab83aac816d9a8d3d80423a593
astroJeff <andrews@astro.columbia.edu>
2016-11-12 08:03:52 +0200

    Added a test

--- src/P_posterior_covar.py
+++ src/P_posterior_covar.py
@@ -137,10 +137,12 @@ def match_binaries(t, sys_start=0, subsample=None, size_integrate_binary=10000, 
         # testing
         # Determine maximum velocity difference as a function of proj_sep_vector
         delta_v_max = np.log10(np.sqrt(c.GGG * 2.0 * c.Msun_to_g / (proj_sep_vector * c.Rsun_to_cm))) - 5.0
-        # delta_v_trans_vector = np.amax(np.vstack([delta_v_trans_vector, min_line(proj_sep_vector)]), axis=0)
-        # ids_good_binary = np.where(P_binary.get_P_binary(proj_sep_vector, delta_v_trans_vector) > 0.0)[0]
+        delta_v_trans_vector = np.amax(np.vstack([delta_v_trans_vector, min_line(proj_sep_vector)]), axis=0)
+        ids_good_binary_2 = np.where(P_binary.get_P_binary(proj_sep_vector, delta_v_trans_vector) > 0.0)[0]
         ids_good_binary = np.where(delta_v_trans_vector < delta_v_max)[0]
         # TESTING
+
+        print i, len(ids_good_binary), len(ids_good_binary_2)
 
         # If no matches, move on
         if len(ids_good_binary) == 0: continue
